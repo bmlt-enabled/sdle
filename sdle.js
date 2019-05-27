3c6d6cbb3cc2a2f6569ba0baf8d0a1bfa8284243
Danny Gershman <danny.gershman@gmail.com>
2019-05-27 00:04:31 -0400

    minor tweaks

--- sdle.js
+++ sdle.js
@@ -130,7 +130,7 @@ function drawServiceBody(id, recurse) {
                 strokeOpacity: 0.8,
                 strokeWeight: 2,
                 fillColor: '#FF0000',
-                fillOpacity: 0.35
+                fillOpacity: 0.5
             });
 
             serviceBodyPolygon.setMap(map);
@@ -147,7 +147,7 @@ function drawServiceBody(id, recurse) {
                 var circle = new google.maps.Circle({
                     map: map,
                     radius: parseFloat($("#willingness").val()) * radius_to_miles_ratio,
-                    fillColor: 'lightblue',
+                    fillColor: 'blue',
                     strokeWeight: 0.5,
                     fillOpacity: 0.05,
                 });
