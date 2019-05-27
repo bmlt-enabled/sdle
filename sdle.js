a7a53ed7732c8fe9aace8a9ba29d832c612764f4
Danny Gershman <danny.gershman@gmail.com>
2019-05-26 22:19:10 -0400

    eh sorted, but not quite what i want

--- sdle.js
+++ sdle.js
@@ -93,17 +93,85 @@ function setMapInfo(pos) {
     });
 }
 
+function Point(lat, lon) {
+    this.x = (lon + 180) * 360;
+    this.y = (lat + 90) * 180;
+    this.latitude = lat;
+    this.longitude = lon;
+
+    this.distance=function(that) {
+        var dX = that.x - this.x;
+        var dY = that.y - this.y;
+        return Math.sqrt((dX*dX) + (dY*dY));
+    };
+
+    this.slope=function(that) {
+        var dX = that.x - this.x;
+        var dY = that.y - this.y;
+        return dY / dX;
+    };
+
+    this.toString=function() {
+        return this.label;
+    };
+}
+
+// Find the upper most point. In case of a tie, get the left most point.
+function upperLeft(points) {
+    var top = points[0];
+    for(var i = 1; i < points.length; i++) {
+        var temp = points[i];
+        if(temp.y > top.y || (temp.y === top.y && temp.x < top.x)) {
+            top = temp;
+        }
+    }
+
+    return top;
+}
+
 function drawServiceBody(id, recurse) {
+    var points = [];
     var service_bodies_coords = [];
     getMeetingsForServiceBody(id, recurse, function(data) {
         for (var i = 0; i < data.length; i++) {
             var meeting = data[i];
-            service_bodies_coords.push({
-                lat: parseFloat(meeting.latitude), lng: parseFloat(meeting.longitude)
-            })
+            points.push(new Point(meeting.latitude, meeting.longitude));
         }
 
-        //service_bodies_coords.push(service_bodies_coords[0]);
+        var upper = upperLeft(points);
+
+        points.sort(function (p1, p2) {
+            // Exclude the 'upper' point from the sort (which should come first).
+            if(p1 === upper) return -1;
+            if(p2 === upper) return 1;
+
+            // Find the slopes of 'p1' and 'p2' when a line is
+            // drawn from those points through the 'upper' point.
+            var m1 = upper.slope(p1);
+            var m2 = upper.slope(p2);
+
+            // 'p1' and 'p2' are on the same line towards 'upper'.
+            if(m1 === m2) {
+                // The point closest to 'upper' will come first.
+                return p1.distance(upper) < p2.distance(upper) ? -1 : 1;
+            }
+
+            // If 'p1' is to the right of 'upper' and 'p2' is the the left.
+            if(m1 <= 0 && m2 > 0) return -1;
+
+            // If 'p1' is to the left of 'upper' and 'p2' is the the right.
+            if(m1 > 0 && m2 <= 0) return 1;
+
+            // It seems that both slopes are either positive, or negative.
+            return m1 > m2 ? -1 : 1;
+        });
+
+        for (var p = 0; p < points.length; p++) {
+            var point = points[p];
+            service_bodies_coords.push({
+                lat: parseFloat(point.latitude), lng: parseFloat(point.longitude)
+            })
+        }
 
         // Construct the polygon.
         var serviceBodyPolygon = new google.maps.Polygon({
